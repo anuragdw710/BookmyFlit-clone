7e56244aa0061ed9005624998653143cbb070117
anuragdw0710 <anuragdw0710@gmail.com>
2024-02-09 12:10:10 +0530

    Setup baseic sequalise

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bodyparser = require("body-parser");
 
 const { PORT } = require('../src/config/serverCongig');
 
 const SetupandStartServer = async () => {
     const app = express();
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({ extended: true }));
     app.listen(PORT, () => {
         console.log(`Server is lissing on ${PORT}`);
     })
